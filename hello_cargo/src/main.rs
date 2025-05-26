55093e7b5b5a6312552c5d48413ad196d9636792
Anton Yarkov <antonyarkov@microsoft.com>
2025-05-26 13:42:24 +0200

    int to uint conversion

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello, world!!");
+    println!("Hello, scopes!");
+
+    let x = 5;
+
+    // XXXX let y = (let z = 6); // Statements do not return value!!! ERROR!
+
+    println!("outer x: {}", x);
+
+    let y = { // An expression!
+        let x = 3;
+        println!("inner x: {}", x);
+        x + 1  // Return value from expression!
+    };
+    
+    println!("outer y: {}", y);
+    println!("outer x: {}", x);
+
+    let sq = square(x);
+
+    let x: u32 = x.try_into().unwrap(); // Required to compare with square
+
+    if sq > x {
+        println!("square x: {}", sq);
+    } else {
+        println!("Something went wrong with square!")
+    }
+
+    let z = if true {
+        1
+    } else {
+        2
+    };
+
+    println!("z: {}", z);
+}
+
+fn square(x: i32) -> u32 {
+    (x * x).try_into().unwrap()
 }
