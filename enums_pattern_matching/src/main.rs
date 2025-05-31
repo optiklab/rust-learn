705d1ca9b6bf191e439ab69344c62d4de1782b0b
Anton Yarkov <anton.yarkov@gmail.com>
2025-05-31 08:54:27 +0200

    pattern matching

--- enums_pattern_matching/src/main.rs
+++ enums_pattern_matching/src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
 }
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
+fn main() {
+    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Nickel);
+    value_in_cents(Coin::Dime);
+    
+    // Prints: State quarter from Alabama!
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+
+    let five = Some(5);
+    println!("{:?}", five); // Prints: Some(5)
+
+    let six = plus_one(five);
+    println!("{:?}", six); // Prints: Some(6)
+
+    let none = plus_one(None);
+    println!("{:?}", none); // Prints: None
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
