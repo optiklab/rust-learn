38df01abf3202b77a0d1f4efc1d647b3e750a497
Anton Yarkov <antonyarkov@microsoft.com>
2025-06-08 13:32:03 +0200

    Traits

--- 17_aggregator/src/lib.rs
+++ 17_aggregator/src/lib.rs
@@ -4,7 +4,15 @@ pub trait Summary {
 
     // With default implementation
     fn summarize(&self) -> String {
-        String::from("(Read more...)")
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+
+    fn summarize_author(&self) -> String {
+
+        // If we don't specify default implementation,
+        // the compiler will require that this method is implemented in
+        // every type that implements the Summary trait, like User.
+        String::from("(unknown author)")
     }
 }
 
@@ -19,6 +27,9 @@ impl Summary for NewsArticle {
     fn summarize(&self) -> String {
         format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
 }
 
 pub struct SocialPost {
@@ -31,6 +42,10 @@ pub struct SocialPost {
 impl Summary for SocialPost {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
+    }
+    
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
 
--- 17_create_traits/src/main.rs
+++ 17_create_traits/src/main.rs
@@ -1,4 +1,58 @@
 use aggregator::{NewsArticle, SocialPost, Summary, User};
+use std::fmt::Display;
+use std::fmt::Debug;
+
+// Summary trait as a parameter.
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+// Same as above, but using a generic type.
+pub fn notify_equal_to_notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+
+//pub fn notify_compare(item1: &impl Summary, item2: &impl Summary) {
+    // This will not compile because the compiler cannot guarantee that
+    // item1 and item2 are of the same type.
+    // println!("Breaking news! {} {}", item1.summarize(), item2.summarize());
+//}
+// Use Traits Bound Syntax to solve:
+pub fn notify_compare<T: Summary>(item1: &T, item2: &T) {
+    // This will compile because we use Traing Bound Syntax to 
+    // let the compiler guarantee that item1 and item2 are of the same type.
+    println!("Compare 2 news: \n 1. {} \n 2. {}", item1.summarize(), item2.summarize());
+}
+
+
+// + syntax to combine multiple traits.
+pub fn notify_multiple(item: &(impl Summary + Display)) {
+}
+pub fn notify_multiple_equal<T: Summary + Display>(item: &T) {
+}
+
+// Instead of
+// fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {
+// }
+// Use where clause:
+fn some_function<T, U>(t: &T, u: &U) -> i32
+    where
+        T: Display + Clone,
+        U: Clone + Debug {
+            0
+}
+
+fn returns_summarizable() -> impl Summary {
+    SocialPost {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        repost: false,
+    }
+    // However, we cannot return multiple types here.
+}
 
 fn main() {
     let post = SocialPost {
@@ -24,11 +78,35 @@ fn main() {
 
     println!("New article available! {}", article.summarize());
     
-    // No specific implementation for User, so it uses the default.
     let user = User {
-        username: String::from("Penguin!"),
+        username: String::from("Penguin"),
         userid: String::from("1"),
         birthdate: String::from("15/03/1990")
     };
+    // No Summary trait implementation for User struct, so it uses the default
+    // and CANNOT point to a specific author.
     println!("Summarize on user: {}", user.summarize());
+
+    notify(&post);
+    notify(&article);
+    notify(&user);
+
+    notify_equal_to_notify(&post);
+    notify_equal_to_notify(&article);
+    notify_equal_to_notify(&user);
+
+    // FOR NOW, this will not compile:
+    //notify_compare(&post, &article);
+    //notify_compare(&article, &user);
+    //notify_compare(&post, &user);
+
+    let article2 = NewsArticle {
+        headline: String::from("Penguins lost in the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins lost again in the NHL.",
+        ),
+    };
+    notify_compare(&article, &article2);
 }
