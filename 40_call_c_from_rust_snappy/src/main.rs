b7127e15b8fb68e0117e175de846fcf3d80401b6
Anton Yarkov <antonyarkov@microsoft.com>
2025-07-11 19:10:27 +0200

    Implement Safe Rust interface over unsafe C library

--- 40_call_c_from_rust_snappy/src/main.rs
+++ 40_call_c_from_rust_snappy/src/main.rs
@@ -1,4 +1,4 @@
-use libc::size_t;
+use libc::{c_int, size_t};
 
 // vcpkg install snappy
 // TODO 
@@ -10,9 +10,104 @@ use libc::size_t;
 #[link(name = "snappy")] // Declare a name of a linking lib: https://github.com/google/snappy/
 unsafe extern "C" {
     fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+
+    fn snappy_compress(input: *const u8,
+                       input_length: size_t,
+                       compressed: *mut u8,
+                       compressed_length: *mut size_t) -> c_int;
+    fn snappy_uncompress(compressed: *const u8,
+                         compressed_length: size_t,
+                         uncompressed: *mut u8,
+                         uncompressed_length: *mut size_t) -> c_int;
+    fn snappy_uncompressed_length(compressed: *const u8,
+                                  compressed_length: size_t,
+                                  result: *mut size_t) -> c_int;
+    fn snappy_validate_compressed_buffer(compressed: *const u8,
+                                         compressed_length: size_t) -> c_int;
 }
 
 fn main() {
+    ////////////////// Calling to C from Rust //////////////////////////////////
     let x = unsafe { snappy_max_compressed_length(100) };
     println!("max compressed length of a 100 byte buffer: {}", x);
+
+    ////////////////// Calling to Safe Rust interface instead //////////////////
+    let d = vec![0xde, 0xad, 0xd0, 0x0d];
+    let c: &[u8] = &compress(&d);
+    let b: bool = validate_compressed_buffer(c);
+    println!("Compressed {}", if b {  "successfully" } else { "not successfully" });
+}
+
+////////////////// Creating Safe Interface over C //////////////////////////////
+
+pub fn validate_compressed_buffer(src: &[u8]) -> bool {
+    unsafe {
+        snappy_validate_compressed_buffer(src.as_ptr(), src.len() as size_t) == 0 // Interfaced C function
+    }
+}
+
+pub fn compress(src: &[u8]) -> Vec<u8> {
+    unsafe {
+        let srclen = src.len() as size_t;
+        let psrc = src.as_ptr();
+
+        let mut dstlen = snappy_max_compressed_length(srclen); // Interfaced C function
+        let mut dst = Vec::with_capacity(dstlen as usize);
+        let pdst = dst.as_mut_ptr();
+
+        snappy_compress(psrc, srclen, pdst, &mut dstlen); // Interfaced C function
+        dst.set_len(dstlen as usize);
+        dst
+    }
+}
+
+pub fn uncompress(src: &[u8]) -> Option<Vec<u8>> {
+    unsafe {
+        let srclen = src.len() as size_t;
+        let psrc = src.as_ptr();
+
+        let mut dstlen: size_t = 0;
+        snappy_uncompressed_length(psrc, srclen, &mut dstlen); // Interfaced C function
+
+        let mut dst = Vec::with_capacity(dstlen as usize);
+        let pdst = dst.as_mut_ptr();
+
+        if snappy_uncompress(psrc, srclen, pdst, &mut dstlen) == 0 { // Interfaced C function
+            dst.set_len(dstlen as usize);
+            Some(dst)
+        } else {
+            None // SNAPPY_INVALID_INPUT
+        }
+    }
+}
+
+////////////// Rust Tests for safe Rust interface of C functions ///////////////
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid() {
+        let d = vec![0xde, 0xad, 0xd0, 0x0d];
+        let c: &[u8] = &compress(&d);
+        assert!(validate_compressed_buffer(c));
+        assert!(uncompress(c) == Some(d));
+    }
+
+    #[test]
+    fn invalid() {
+        let d = vec![0, 0, 0, 0];
+        assert!(!validate_compressed_buffer(&d));
+        assert!(uncompress(&d).is_none());
+    }
+
+    #[test]
+    fn empty() {
+        let d = vec![];
+        assert!(!validate_compressed_buffer(&d));
+        assert!(uncompress(&d).is_none());
+        let c = compress(&d);
+        assert!(validate_compressed_buffer(&c));
+        assert!(uncompress(&c) == Some(d));
+    }
 }
