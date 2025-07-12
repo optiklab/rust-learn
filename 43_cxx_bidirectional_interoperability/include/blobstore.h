63a307a5265756592b759cbe438802191da035de
Anton Yarkov <antonyarkov@microsoft.com>
2025-07-12 21:14:29 +0200

    Refactoring

--- 43_cxx_bidirectional_interoperability/include/blobstore.h
+++ 43_cxx_bidirectional_interoperability/include/blobstore.h
@@ -13,8 +13,16 @@ public:
   BlobMetadata metadata(uint64_t blobid) const;
   
 private:
-  class Implementation;
-  std::shared_ptr<Implementation> pimpl; // unique_ptr actually preferred by Google style guide
+  // Below, we are using a common C++ idiom called the 
+  // "Pimpl" (Pointer to Implementation) pattern. It hides the implementation 
+  // details of BlobstoreClient from users of the header, improving 
+  // encapsulation and reducing compile-time dependencies.
+
+  class Implementation; // A forward declaration of a nested class
+
+  // A smart pointer to an instance of that Implementation class
+  // However, unique_ptr actually preferred by Google style guide.
+  std::shared_ptr<Implementation> pimpl; 
 };
 
 std::unique_ptr<BlobstoreClient> new_blobstore_client();
