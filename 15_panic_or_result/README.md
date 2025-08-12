622f41d763c260e5b2cecaf8716f1adf241a2509
Anton Yarkov <antonyarkov@microsoft.com>
2025-08-12 13:47:34 +0200

    Implemented TryFrom

--- 15_panic_or_result/README.md
+++ 15_panic_or_result/README.md
@@ -131,10 +131,8 @@ match s.parse_u32() {
 
 ## Error reporting
 
-In the previous exercise you had to destructure the `TitleError` variant to extract the error message and
-pass it to the `panic!` macro.\
-This is a (rudimentary) example of **error reporting**: transforming an error type into a representation that can be
-shown to a user, a service operator, or a developer.
+In the previous exercise you had to destructure the `TitleError` (see new_alt() on line 237) variant to extract the error message and
+pass it to the `panic!` macro. This is a (rudimentary) example of **error reporting**: transforming an error type into a representation that can be shown to a user, a service operator, or a developer.
 
 It's not practical for each Rust developer to come up with their own error reporting strategy: it'd be a waste of time
 and it wouldn't compose well across projects.
@@ -181,3 +179,229 @@ That's why `Debug` can be automatically implemented using the `#[derive(Debug)]`
 **requires** a manual implementation.
 
 EVERY ERROR ENUM HAVE TO IMLPEMENT ERROR TRAIT.
+See TicketNewError on line 188.
+
+# `thiserror`
+
+## Custom error types
+
+We've seen how to implement the `Error` trait "manually" for a custom error type.\
+Imagine that you have to do this for most error types in your codebase. That's a lot of boilerplate, isn't it?
+
+We can remove some of the boilerplate by using [`thiserror`](https://docs.rs/thiserror/latest/thiserror/),
+a Rust crate that provides a **procedural macro** to simplify the creation of custom error types.
+
+```rust
+#[derive(thiserror::Error, Debug)]
+enum TicketNewError {
+    #[error("{0}")]
+    TitleError(String),
+    #[error("{0}")]
+    DescriptionError(String),
+}
+```
+
+## You can write your own macros
+
+All the `derive` macros we've seen so far were provided by the Rust standard library.\
+`thiserror::Error` is the first example of a **third-party** `derive` macro.
+
+`derive` macros are a subset of **procedural macros**, a way to generate Rust code at compile time.
+We won't get into the details of how to write a procedural macro in this course, but it's important
+to know that you can write your own!\
+A topic to approach in a more advanced Rust course.
+
+## Custom syntax
+
+Each procedural macro can define its own syntax, which is usually explained in the crate's documentation.
+In the case of `thiserror`, we have:
+
+- `#[derive(thiserror::Error)]`: this is the syntax to derive the `Error` trait for a custom error type, helped by `thiserror`.
+- `#[error("{0}")]`: this is the syntax to define a `Display` implementation for each variant of the custom error type.
+  `{0}` is replaced by the zero-th field of the variant (`String`, in this case) when the error is displayed.
+
+# `From` and `Into`
+
+What `.into()` is doing here?
+
+```rust
+let ticket = Ticket::new(
+    "A title".into(), 
+    "A description".into(), 
+    "To-Do".into()
+);
+```
+
+This is the signature of the `new` method:
+
+```rust
+impl Ticket {
+    pub fn new(
+        title: String, 
+        description: String, 
+        status: String
+    ) -> Self {
+        // [...]
+    }
+}
+```
+
+We've also seen that string literals (such as `"A title"`) are of type `&str`.
+We have a type mismatch here: a `String` is expected, but we have a `&str`.
+No magical coercion will come to save us this time; we need **to perform a conversion**.
+
+## `From` and `Into`
+
+The Rust standard library defines two traits for **infallible conversions**: `From` and `Into`,
+in the `std::convert` module.
+
+```rust
+pub trait From<T>: Sized {
+    fn from(value: T) -> Self;
+}
+
+pub trait Into<T>: Sized {
+    fn into(self) -> T;
+}
+```
+
+These trait definitions showcase a few concepts that we haven't seen before: **supertraits** and **implicit trait bounds**.
+Let's unpack those first.
+
+### Supertrait / Subtrait
+
+The `From: Sized` syntax implies that `From` is a **subtrait** of `Sized`: any type that
+implements `From` must also implement `Sized`.
+Alternatively, you could say that `Sized` is a **supertrait** of `From`.
+
+### Implicit trait bounds
+
+Every time you have a generic type parameter, the compiler implicitly assumes that it's `Sized`.
+
+For example:
+
+```rust
+pub struct Foo<T> {
+    inner: T,
+}
+```
+
+is actually equivalent to:
+
+```rust
+pub struct Foo<T: Sized> 
+{
+    inner: T,
+}
+```
+
+In the case of `From<T>`, the trait definition is equivalent to:
+
+```rust
+pub trait From<T: Sized>: Sized {
+    fn from(value: T) -> Self;
+}
+```
+
+In other words, _both_ `T` and the type implementing `From<T>` must be `Sized`, even
+though the former bound is implicit.
+
+### Negative trait bounds
+
+You can opt out of the implicit `Sized` bound with a **negative trait bound**:
+
+```rust
+pub struct Foo<T: ?Sized> {
+    //            ^^^^^^^
+    //            This is a negative trait bound
+    inner: T,
+}
+```
+
+This syntax reads as "`T` may or may not be `Sized`", and it allows you to
+bind `T` to a DST (e.g. `Foo<str>`). It is a special case, though: negative trait bounds are exclusive to `Sized`,
+you can't use them with other traits.
+
+## `&str` to `String`
+
+In [`std`'s documentation](https://doc.rust-lang.org/std/convert/trait.From.html#implementors)
+you can see which `std` types implement the `From` trait.\
+You'll find that `String` implements `From<&str> for String`. Thus, we can write:
+
+```rust
+let title = String::from("A title");
+```
+
+We've been primarily using `.into()`, though.\
+If you check out the [implementors of `Into`](https://doc.rust-lang.org/std/convert/trait.Into.html#implementors)
+you won't find `Into<String> for &str`. What's going on?
+
+`From` and `Into` are **dual traits**.\
+In particular, `Into` is implemented for any type that implements `From` using a **blanket implementation**:
+
+```rust
+impl<T, U> Into<U> for T
+where
+    U: From<T>,
+{
+    fn into(self) -> U {
+        U::from(self)
+    }
+}
+```
+
+If a type `U` implements `From<T>`, then `Into<U> for T` is automatically implemented. That's why
+we can write `let title = "A title".into();`.
+
+## `.into()`
+
+Every time you see `.into()`, you're witnessing a conversion between types.\
+What's the target type, though?
+
+In most cases, the target type is either:
+
+- Specified by the signature of a function/method (e.g. `Ticket::new` in our example above)
+- Specified in the variable declaration with a type annotation (e.g. `let title: String = "A title".into();`)
+
+`.into()` will work out of the box as long as the compiler can infer the target type from the context without ambiguity.
+
+# `TryFrom` and `TryInto`
+
+In the previous chapter we looked at the [`From` and `Into` traits](../04_traits/09_from.md),
+Rust's idiomatic interfaces for **infallible** type conversions.\
+But what if the conversion is not guaranteed to succeed?
+
+We now know enough about errors to discuss the **fallible** counterparts of `From` and `Into`:
+`TryFrom` and `TryInto`.
+
+## `TryFrom` and `TryInto`
+
+Both `TryFrom` and `TryInto` are defined in the `std::convert` module, just like `From` and `Into`.
+
+```rust
+pub trait TryFrom<T>: Sized {
+    type Error;
+    fn try_from(value: T) -> Result<Self, Self::Error>;
+}
+
+pub trait TryInto<T>: Sized {
+    type Error;
+    fn try_into(self) -> Result<T, Self::Error>;
+}
+```
+
+The main difference between `From`/`Into` and `TryFrom`/`TryInto` is that the latter return a `Result` type.\
+This allows the conversion to fail, returning an error instead of panicking.
+
+## `Self::Error`
+
+Both `TryFrom` and `TryInto` have an associated `Error` type.
+This allows each implementation to specify its own error type, ideally the most appropriate for the conversion
+being attempted.
+
+`Self::Error` is a way to refer to the `Error` associated type defined in the trait itself.
+
+## Duality
+
+Just like `From` and `Into`, `TryFrom` and `TryInto` are dual traits.\
+If you implement `TryFrom` for a type, you get `TryInto` for free.
--- 15_panic_or_result/src/main.rs
+++ 15_panic_or_result/src/main.rs
@@ -8,6 +8,7 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::fmt; // For Error + Debug + Display trait.
 use std::error::Error; // For Error trait.
+use thiserror::Error;
 
 fn read_username_from_file() -> Result<String, io::Error> {
     let username_file_result = File::open("hello.txt");
@@ -75,7 +76,7 @@ fn main() {
     };
     */
 
-    let greeting_file2 = match greeting_file_result {
+    let _greeting_file2 = match greeting_file_result {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
@@ -88,7 +89,7 @@ fn main() {
         },
     };
 
-    let greeting_file3 = File::open("hello.txt").unwrap_or_else(|error| {
+    let _greeting_file3 = File::open("hello.txt").unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
             File::create("hello.txt").unwrap_or_else(|error| {
                 panic!("Problem creating the file: {error:?}");
@@ -101,15 +102,15 @@ fn main() {
     // Shortcuts:
 
     // unwrap already doing that for us
-    let greeting_file4 = File::open("hello.txt").unwrap();
+    let _greeting_file4 = File::open("hello.txt").unwrap();
     
     // expect is similar to unwrap, but allows you to provide a custom panic message.
-    let greeting_file5 = File::open("hello.txt")
+    let _greeting_file5 = File::open("hello.txt")
         .expect("hello.txt should be included in this project");
 
     //let greeting_file = File::open("hello.txt")?; // Will not compile
     
-    let home: IpAddr = "127.0.0.1"
+    let _home: IpAddr = "127.0.0.1"
         .parse()
         .expect("Hardcoded IP address should be valid");
 
@@ -185,6 +186,10 @@ enum Status {
     Done,
 }
 
+// Implementing `Debug`, `Display` and `Error` for the `TicketNewError` enum.
+//  When implementing `Display`, you may want to use the `write!` macro from Rust's standard library.
+//  The docs for the `std::fmt` module are a good place to start and look for examples:
+//  https://doc.rust-lang.org/std/fmt/index.html#write
 #[derive(Debug)]
 enum TicketNewError {
     TitleError { description: String },
@@ -263,7 +268,10 @@ impl Ticket {
     }
 }
 
-
+// `easy_ticket` should panic when the title is invalid, using the error message
+//   stored inside the relevant variant of the `TicketNewError` enum.
+//   When the description is invalid, instead, it should use a default description:
+//   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
     if title.is_empty() {
         panic!("Title cannot be empty");
@@ -283,6 +291,161 @@ fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
         status,
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////// End of Error Trait //////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////     `thiserror`    //////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// Cargo.toml
+// [dependencies]
+// thiserror = "2"
+
+// We've seen how to implement the `Error` trait "manually" for a custom error type.
+// Imagine that you have to do this for most error types in your codebase. That's a lot of boilerplate, isn't it?
+// We can remove some of the boilerplate by using [`thiserror`](https://docs.rs/thiserror/latest/thiserror/),
+// a Rust crate that provides a **procedural macro** to simplify the creation of custom error types.
+
+// TODO: Implement the `Error` trait for `TicketNewError` using `thiserror`.
+//   We've changed the enum variants to be more specific, thus removing the need for storing
+//   a `String` field into each variant.
+//   You'll also have to add `thiserror` as a dependency in the `Cargo.toml` file.
+
+#[derive(Error, Debug)]
+enum TicketNewError2 {
+    #[error("Title cannot be empty")]
+    TitleCannotBeEmpty,
+    #[error("Title cannot be longer than 50 bytes")]
+    TitleTooLong,
+    #[error("Description cannot be empty")]
+    DescriptionCannotBeEmpty,
+    #[error("Description cannot be longer than 500 bytes")]
+    DescriptionTooLong,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct Ticket2 {
+    title: String,
+    description: String,
+    status: Status2,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Status2 {
+    ToDo,
+    InProgress { assigned_to: String },
+    Done,
+}
+
+impl Ticket2 {
+    pub fn new(
+        title: String,
+        description: String,
+        status: Status2,
+    ) -> Result<Ticket2, TicketNewError2> {
+        if title.is_empty() {
+            return Err(TicketNewError2::TitleCannotBeEmpty);
+        }
+        if title.len() > 50 {
+            return Err(TicketNewError2::TitleTooLong);
+        }
+        if description.is_empty() {
+            return Err(TicketNewError2::DescriptionCannotBeEmpty);
+        }
+        if description.len() > 500 {
+            return Err(TicketNewError2::DescriptionTooLong);
+        }
+
+        Ok(Ticket2 {
+            title,
+            description,
+            status,
+        })
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////    End of `thiserror`    ////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////   Into() & From()  //////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// Implement `TryFrom<String>` and `TryFrom<&str>` for `Status`.
+// The parsing should be case-insensitive.
+
+#[derive(Error, Debug)]
+enum TicketError3 {
+    #[error("Ticked status is undefined.")]
+    SomeError
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Status3 {
+    ToDo,
+    InProgress,
+    Done,
+}
+
+impl TryFrom<String> for Status3 {
+    type Error = TicketError3; 
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Status3::try_from(value.as_str())
+    }
+}
+
+impl TryFrom<&str> for Status3 {
+    type Error = TicketError3; 
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status3::ToDo),
+            "inprogress" => Ok(Status3::InProgress),
+            "done" => Ok(Status3::Done),
+            _ => Err(TicketError3::SomeError)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests3 {
+    use super::*;
+    use std::convert::TryFrom;
+
+    #[test]
+    fn test_try_from_string() {
+        let status = Status3::try_from("ToDO".to_string()).unwrap();
+        assert_eq!(status, Status3::ToDo);
+
+        let status = Status3::try_from("inproGress".to_string()).unwrap();
+        assert_eq!(status, Status3::InProgress);
+
+        let status = Status3::try_from("Done".to_string()).unwrap();
+        assert_eq!(status, Status3::Done);
+    }
+
+    #[test]
+    fn test_try_from_str() {
+        let status = Status3::try_from("todo").unwrap();
+        assert_eq!(status, Status3::ToDo);
+
+        let status = Status3::try_from("inprogress").unwrap();
+        assert_eq!(status, Status3::InProgress);
+
+        let status = Status3::try_from("done").unwrap();
+        assert_eq!(status, Status3::Done);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////  End of Into() & From()  //////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 #[cfg(test)]
 mod tests {
@@ -370,5 +533,37 @@ mod tests {
 
     pub fn valid_description() -> String {
         "A description".into()
+    }
+
+    // End of Helpers
+    
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////// this error tests ////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+    #[test]
+    fn title2_cannot_be_empty() {
+        let err = Ticket2::new("".into(), valid_description(), Status2::ToDo).unwrap_err();
+        assert_eq!(err.to_string(), "Title cannot be empty");
+    }
+
+    #[test]
+    fn description2_cannot_be_empty() {
+        let err = Ticket2::new(valid_title(), "".into(), Status2::ToDo).unwrap_err();
+        assert_eq!(err.to_string(), "Description cannot be empty");
+    }
+
+    #[test]
+    fn title2_cannot_be_longer_than_fifty_chars() {
+        let err = Ticket2::new(overly_long_title(), valid_description(), Status2::ToDo).unwrap_err();
+        assert_eq!(err.to_string(), "Title cannot be longer than 50 bytes");
+    }
+
+    #[test]
+    fn description2_cannot_be_too_long() {
+        let err = Ticket2::new(valid_title(), overly_long_description(), Status2::ToDo).unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            "Description cannot be longer than 500 bytes"
+        );
     }
 }
